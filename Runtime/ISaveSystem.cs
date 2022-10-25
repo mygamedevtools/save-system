560e5ae491d86a7c9431a48a05c04f6cec4f9ddb
João Borks <joao.borks@gmail.com>
2022-10-25 20:45:06 -0300

    chore: Add base files

---
+++ Runtime/ISaveSystem.cs
@@ -0,0 +1,13 @@
+/**
+ * ISaveSystem.cs
+ * Created by: João Borks [joao.borks@gmail.com]
+ * Created on: 2022-10-25
+ */
+
+namespace MyGameDevTools.SaveSystem
+{
+    public interface ISaveSystem
+    {
+        
+    }
+}
